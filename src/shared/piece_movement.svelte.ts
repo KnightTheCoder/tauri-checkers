ba090102e58fee5af1e0b192da98493bbeb7f994
KnightTheCoder <90042747+KnightTheCoder@users.noreply.github.com>
2024-11-05 19:25:15 +0100

    can take other player's pieces now

--- src/shared/piece_movement.svelte.ts
+++ src/shared/piece_movement.svelte.ts
@@ -1,3 +1,4 @@
+import { abs } from './utils';
 import { getNextPlayerName, nextPlayer } from './player_turns.svelte';
 import { getBoardStatus } from './board_logic.svelte';
 
@@ -35,20 +36,47 @@ function movePiece(player: string, position: PositionType, isKing: boolean) {
     };
 
     // X is vertical axis, only 1 directional movement is checked
-    if (movement.from.isKing) {
-      issueMovement();
-    } else if (
-      (moveDistance.x == 1 && nextPlayer == 'white') ||
-      (moveDistance.x == -1 && nextPlayer == 'black')
-    ) {
-      issueMovement();
+    if (abs(moveDistance.x) == 1 && abs(moveDistance.y) == 1) {
+      if (movement.from.isKing) {
+        issueMovement();
+      } else if (
+        (moveDistance.x == 1 && nextPlayer == 'white') ||
+        (moveDistance.x == -1 && nextPlayer == 'black')
+      ) {
+        issueMovement();
+      }
+    } else if (abs(moveDistance.x) == 2 && abs(moveDistance.y) == 2) {
+      let foundPlayer = isPlayerPresent();
+
+      if (!foundPlayer) {
+        resetAll();
+        return;
+      }
+
+      if (movement.from.isKing) {
+        issueMovement(foundPlayer);
+      } else if (
+        (moveDistance.x == 2 && nextPlayer == 'white') ||
+        (moveDistance.x == -2 && nextPlayer == 'black')
+      ) {
+        issueMovement(foundPlayer);
+      }
     }
 
     resetAll();
   }
 }
 
-function issueMovement() {
+function resetAll() {
+  movement.from = null;
+  movement.to = null;
+}
+
+function getMovementStatus() {
+  return $state.snapshot(movement);
+}
+
+function issueMovement(playerLocation: PositionType | undefined = undefined) {
   let movement = getMovementStatus();
   let from = movement.from;
   let to = movement.to;
@@ -70,7 +98,7 @@ function issueMovement() {
     is_background: false,
     player: '',
     position: from.position,
-    isKing: from.isKing
+    isKing: false
   };
 
   board[to.position.x][to.position.y] = {
@@ -80,16 +108,37 @@ function issueMovement() {
     isKing: reachedEnd
   };
 
+  if (playerLocation) {
+    board[playerLocation.x][playerLocation.y] = {
+      is_background: false,
+      player: '',
+      position: playerLocation,
+      isKing: false
+    };
+  }
+
   nextPlayer();
 }
 
-function resetAll() {
-  movement.from = null;
-  movement.to = null;
-}
+function isPlayerPresent(): PositionType | undefined {
+  let x = 0;
+  let y = 0;
 
-function getMovementStatus() {
-  return $state.snapshot(movement);
+  if (!movement.from || !movement.to) return;
+
+  if (movement.from.position.x > movement.to.position.x) {
+    x = movement.to.position.x + 1;
+  } else {
+    x = movement.to.position.x - 1;
+  }
+
+  if (movement.from.position.y > movement.to.position.y) {
+    y = movement.to.position.y + 1;
+  } else {
+    y = movement.to.position.y - 1;
+  }
+
+  return { x, y };
 }
 
 export { getMovementStatus, movePiece, resetAll };
---
+++ src/shared/utils.ts
@@ -0,0 +1,9 @@
+function abs(numb: number): number {
+  if (numb >= 0) {
+    return numb;
+  } else {
+    return -1 * numb;
+  }
+}
+
+export { abs };
